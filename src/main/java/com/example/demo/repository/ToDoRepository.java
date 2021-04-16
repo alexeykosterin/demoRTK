26cb367ca40ef2ee7e22cf6418b63d8382874a65
alexeykosterin <44132959+alexeykosterin@users.noreply.github.com>
2021-04-16 13:52:20 +0700

    add group

--- src/main/java/com/example/demo/repository/ToDoRepository.java
+++ src/main/java/com/example/demo/repository/ToDoRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Todo;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ToDoRepository extends CrudRepository<Todo, String> {
+public interface ToDoRepository extends JpaRepository<Todo, Long> {
 }
